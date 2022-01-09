d82ccc2ad95b462d9171ffc90ce974145d0007f7
Philippe Anel <philippe.anel@mediawen.com>
2022-01-09 17:39:03 +0100

    make it compile on MacOS X too.

--- build-macosx.sh
+++ build-macosx.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 cargo build
 
-echo mac os x not supported yet
-# c++ -std=c++11 -Wall -o stigmark-client -Iinclude examples/stigmark-client/main.cc examples/stigmark-client/echo.cc examples/stigmark-client/login.cc examples/stigmark-client/collection.cc target/debug/libstigmark_client.a
+c++ -std=c++11 -Wall \
+    -framework CoreServices \
+    -framework Security \
+    -o stigmark-client \
+    -Iinclude \
+    examples/stigmark-client/main.cc \
+    examples/stigmark-client/echo.cc \
+    examples/stigmark-client/login.cc \
+    examples/stigmark-client/collection.cc \
+    target/debug/libstigmark_client.a

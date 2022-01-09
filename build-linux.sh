b1c900afd57f3d5adaa9ea7dd42ef85ded42aa93
Philippe Anel <philippe.anel@mediawen.com>
2022-01-09 16:22:21 +0100

    updated build scripts

---
+++ build-linux.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+OPENSSL_STATIC=1 cargo build
+c++ -std=c++11 -Wall -o stigmark-client \
+    -Iinclude \
+    examples/stigmark-client/main.cc \
+    examples/stigmark-client/echo.cc \
+    examples/stigmark-client/login.cc \
+    examples/stigmark-client/collection.cc \
+    target/debug/libstigmark_client.a \
+    -pthread -ldl -lssl -lcrypto
+
---
+++ build-macosx.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cargo build
+
+echo mac os x not supported yet
+# c++ -std=c++11 -Wall -o stigmark-client -Iinclude examples/stigmark-client/main.cc examples/stigmark-client/echo.cc examples/stigmark-client/login.cc examples/stigmark-client/collection.cc target/debug/libstigmark_client.a

ab4b404d14a145e8f4a5a484fe8d644202b0f16d
Philippe Anel <zexigh@gmail.com>
2022-01-19 14:19:58 +0100

    added godot::array

--- src-stigmarkapp/main.gd
+++ src-stigmarkapp/main.gd
@@ -3,5 +3,7 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	var s = Stigmark.new()
-	print(s.login("hello", "world"))
+	var urls = s.search("mariadb")
+	for url in urls:
+		print(url)
 	pass # Replace with function body.

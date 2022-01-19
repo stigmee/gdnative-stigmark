d86dc12767ea347896c7e13808cc888a7fc1a4a9
Philippe Anel <zexigh@gmail.com>
2022-01-19 14:22:03 +0100

    added missing license in c++ comments

--- src-stigmarkapp/main.gd
+++ src-stigmarkapp/main.gd
@@ -1,3 +1,26 @@
+#
+#  Stigmee: A 3D browser and decentralized social network.
+#  Copyright 2021-2022 Philippe Anel <zexigh@gmail.com>
+#
+#  This file is part of Stigmee.
+#
+#  Project : Stigmark
+#  Version : 0.0-1
+#
+#  Stigmee is free software: you can redistribute it and/or modify it
+#  under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful, but
+#  WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#  General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
 extends Node2D
 
 # Called when the node enters the scene tree for the first time.

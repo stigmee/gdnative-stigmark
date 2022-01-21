8e6430647976ca0b252e3e20a18b537e5e966c0f
Philippe Anel <zexigh@gmail.com>
2022-01-21 09:35:35 +0100

    forgot search.cc

--- build-linux.sh
+++ build-linux.sh
@@ -3,6 +3,7 @@ OPENSSL_STATIC=1 cargo build
 c++ -std=c++11 -Wall -o stigmark-client \
     -Iinclude \
     examples/stigmark-client/main.cc \
+    examples/stigmark-client/search.cc \
     examples/stigmark-client/echo.cc \
     examples/stigmark-client/login.cc \
     examples/stigmark-client/collection.cc \

e62fd0dd12dd6c26e7887230ac249d43eb5f743b
Quentin Quadrat <lecrapouille@gmail.com>
2022-01-30 00:25:49 +0100

    Cleaning Stigmark SConstruct
    Make it consistent with Godot/CEF SConstruct.
    Get from command line stigmee workspace path and godot version.
    Not more script to download locally godot-cpp

--- src-stigmarkmod/build-win64.cmd
+++
@@ -1,5 +0,0 @@
-git clone --recursive -b 3.4 https://github.com/godotengine/godot-cpp
-mkdir project
-mkdir project\gdnative
-scons platform=windows
-xcopy /y project\gdnative\windows\libstigmark.dll ..\src-stigmarkapp\bin\win64

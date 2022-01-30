d69eaf3f9a70c833401bb4b96e97f9ac3d0c582f
Alain D <codeur_fou@hotmail.fr>
2022-01-30 18:43:25 +0100

    changing lib name
    changing lib name to libstigmark_client.lib

--- build-windows.cmd
+++ build-windows.cmd
@@ -1,2 +1,2 @@
 cargo build
-cl /EHsc /Zi /Fe:stigmark-client.exe /Iinclude examples\stigmark-client\main.cc examples\stigmark-client\echo.cc examples\stigmark-client\login.cc examples\stigmark-client\search.cc examples\stigmark-client\collection.cc target\debug\stigmark_client.lib
+cl /EHsc /Zi /Fe:stigmark-client.exe /Iinclude examples\stigmark-client\main.cc examples\stigmark-client\echo.cc examples\stigmark-client\login.cc examples\stigmark-client\search.cc examples\stigmark-client\collection.cc target\debug\libstigmark_client.lib

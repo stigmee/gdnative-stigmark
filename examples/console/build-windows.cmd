6297d0cd9b82243625cad7467d4811421e45d1a7
Quentin Quadrat <lecrapouille@gmail.com>
2022-05-28 09:24:32 +0200

    Windows: Append lib to stigmark_client.lib

--- examples/console/build-windows.cmd
+++ examples/console/build-windows.cmd
@@ -1,2 +1,9 @@
 cargo build
-cl /EHsc /Zi /Fe:stigmark-client.exe /I..\..\include main.cc echo.cc login.cc search.cc collection.cc ..\..\target\debug\stigmark_client.lib
+
+set here=%cd%
+cd ..\..\target\debug
+ren stigmark_client.lib libstigmark_client.lib
+ren stigmark_client.dll libstigmark_client.dll
+cd %here%
+
+cl /EHsc /Zi /Fe:stigmark-client.exe /I..\..\include main.cc echo.cc login.cc search.cc collection.cc ..\..\target\debug\libstigmark_client.lib
